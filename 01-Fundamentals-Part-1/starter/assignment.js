71e7e1cd994fa1ab3de8395b0bcf547c97ed0270
Gülşah <gulsahtuncay1@gmail.com>
2023-09-11 20:29:26 +0300

    Assignment-1

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,9 @@
+/* LECTURE: Values and Variables */
+
+let country = "Turkey";
+let continent = "Asia and Europe";
+let population = 84;
+
+console.log(country);
+console.log(continent);
+console.log(population);
