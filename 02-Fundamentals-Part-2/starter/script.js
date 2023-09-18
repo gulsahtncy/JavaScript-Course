bd60d14bc836a37e9d6fa2e9a1e911bcb386a050
Gülşah <gulsahtuncay1@gmail.com>
2023-09-18 23:21:15 +0300

    Adding new file

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,10 +1,19 @@
 "use strict";
 
 function logger() {
-    console.log("My name is Jonas");
+  console.log("My name is Jonas");
 }
 
 //calling / running / invoking function
 logger();
 logger();
 logger();
+
+function fruitProcessor(apples, oranges) {
+  // console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oeanges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
