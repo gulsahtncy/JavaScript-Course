adbdfc54f031605ec88b62356edaf173a8c587ff
Gülşah <gulsahtuncay1@gmail.com>
2023-09-20 22:21:29 +0300

    Adding new file

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -10,10 +10,17 @@ logger();
 logger();
 
 function fruitProcessor(apples, oranges) {
-  // console.log(apples, oranges);
-  const juice = `Juice with ${apples} apples and ${oeanges} oranges.`;
+  //console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
   return juice;
 }
 
+fruitProcessor(5, 0);
+
 const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
+
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
