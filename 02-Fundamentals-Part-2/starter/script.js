fa609cc222acec66f46b5362228bcf7ff6e853c7
Gülşah <gulsahtuncay1@gmail.com>
2023-09-21 17:11:42 +0300

    Adding new file

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function logger() {
+/*function logger() {
   console.log("My name is Jonas");
 }
 
@@ -36,10 +36,43 @@ const age1 = calcAge1(1991);
 //console.log(age1);
 
 //function expression
-const calcAge2 = function (birthYear) /*anonymous function*/ {
+const calcAge2 = function (birthYear) anonymous function {
   return 2037 - birthYear;
 };
 
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+
+//arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //return retirement
+  return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
+*/
+
+//Function Calling other Function
+/*function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} apple and ${orangePieces} orange.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));*/
+
+  
