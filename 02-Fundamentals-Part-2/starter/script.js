e9240f1b41ac5f981f4a53a979834f8f9531022b
Gülşah <gulsahtuncay1@gmail.com>
2023-10-03 23:32:33 +0300

    Adding new

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -192,3 +192,19 @@ const jonas = {
   friends: ['Michael', 'Peter', 'Steven']
 };
 
+// Dot vs. Bracket Notation
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
