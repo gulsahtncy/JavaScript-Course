a9c24ba9fd2ad077aeb1076832b63ccaa3522db7
Gülşah <gulsahtuncay1@gmail.com>
2023-10-18 23:53:29 +0300

    Adding new file

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -256,7 +256,7 @@ console.log(jonas.age);*/
 //Challenge
 // " Jonas is a 46-year old teacher, and he has a driver's icense "
 
-const jonas = {
+/*const jonas = {
   firstName: "Jonas",
   lastName: "Schmedtmann",
   birthYear: 1991,
@@ -276,4 +276,78 @@ const jonas = {
   },
 };
 
-console.log(jonas.getSummary());
+console.log(jonas.getSummary());*/
+
+/*for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}*/
+
+/*const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+  //Reading from jonas array
+  console.log(jonas[i], typeof jonas[i]);
+
+  //Filling types array
+  //types[i] = typeof jonas[i];
+  types.push[typeof jonas[i]];
+}
+console.log(types);*/
+
+/*const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for(let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);*/
+
+//continue and break
+/*const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+
+console.log("--- ONLY STRINGS ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") continue;
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log("--- BREAK WITH NUMBER ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === "number") break;
+  console.log(jonas[i], typeof jonas[i]);
+}*/
+
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-----Starting exercise ${exercise}`);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+  }
+}
