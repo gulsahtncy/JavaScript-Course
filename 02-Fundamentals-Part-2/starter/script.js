21716b066a86983bc2c8bfdb803d7b4059a16e07
Gülşah <gulsahtuncay1@gmail.com>
2023-10-19 22:29:23 +0300

    Adding new file

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -332,7 +332,7 @@ for (let i = 0; i < jonas.length; i++) {
   console.log(jonas[i], typeof jonas[i]);
 }*/
 
-const jonas = [
+/*const jonas = [
   "Jonas",
   "Schmedtmann",
   2037 - 1991,
@@ -344,10 +344,26 @@ for (let i = jonas.length - 1; i >= 0; i--) {
   console.log(i, jonas[i]);
 }
 
-
 for (let exercise = 1; exercise < 4; exercise++) {
   console.log(`-----Starting exercise ${exercise}`);
   for (let rep = 1; rep < 6; rep++) {
     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
   }
+}*/
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+
+let rep = 1;
+while (rep <= 10) {
+  console.log(`Lifting weights repetition ${rep}`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to end...");
 }
